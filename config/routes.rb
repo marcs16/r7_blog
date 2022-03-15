15c58cd6dc651b4650c3261a2e5325a45e36bc8c
marcs16 <luis.edo1234@gmail.com>
2022-03-14 22:02:25 -0500

    change routes for handling devise to  users controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   resources :posts
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+  }
+  
   # Defines the root path route ("/")
   root "pages#home"
 end
