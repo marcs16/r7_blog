82e63ca56d27386b571d016d9fabaac5328a62dd
marcs16 <luis.edo1234@gmail.com>
2022-03-16 23:34:40 -0500

    Add comments for post, delete and create actions and action text

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: {minimum:2, maximum:50}
     validates :body, presence: true, length: {minimum:2, maximum: 2000} 
     belongs_to :user
+    has_many :comments, dependent: :destroy 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'users/profile'
   get '/u/:id', to: 'users#profile', as: 'user'
-  resources :posts
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,6 +9,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     passwords: 'users/passwords',
   }
+
+   resources :posts do
+    resources :comments
+  end
   
   # Defines the root path route ("/")
   root "pages#home"
